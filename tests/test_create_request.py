bd82d3dbafb2c9e9f010b87aeecfde16d3580d74
Tomáš Novotný <tomas.novotny@synetech.cz>
2021-04-03 00:47:09 +0200

    Enhance processing
    Enhance processing
    
    Enhance processing

--- tests/test_create_request.py
+++ tests/test_create_request.py
@@ -1,3 +1,4 @@
+from curl2swift.parsing.parse_context import RequestProperties
 from tests.test_request import TEST_REQUEST
 from curl2swift.parsing.get_parser import get_curl_parser
 from curl2swift.processing.create_request import create_request
@@ -7,5 +8,6 @@ CURL = """curl -i https://api.github.com/users/defunkt"""
 
 def test_create_request():
     parser = get_curl_parser()
-    request = create_request(CURL, parser)
+    context = RequestProperties('get', 'https://api.github.com/users/defunkt', None, None, None, None, None, ())
+    request = create_request(context)
     assert request == TEST_REQUEST.strip()
