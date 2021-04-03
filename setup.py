bba4762602cc1094f3d27809cc1d08b58f073421
Tomáš Novotný <tomas.novotny@synetech.cz>
2021-04-03 23:49:46 +0200

    VERSION 0.3.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 # circleci.py version
-VERSION = "0.3.3"
+VERSION = "0.3.4"
 
 class VerifyVersionCommand(install):
     """Custom command to verify that the git tag matches our version"""
